83d33b2229a5fae8632caa150df6b971c2978ac9
liyang <liyang0510@sina.com>
2017-04-19 20:38:11 +0800

    新增切换城市和手动更新天气的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by ly on 2017-04-19.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
